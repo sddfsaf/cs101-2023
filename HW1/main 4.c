19f9bbd97fa99129afc2bda79574ffb1f6b63e38
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2023-02-27 16:31:58 +0800

    Update main 4.c

--- HW1/main 4.c
+++ HW1/main 4.c
@@ -1,27 +1,19 @@
 
 
+
+
 #include <stdio.h>
 
 
-void find_num (char a[]){
+void
+find_num (char a[])
+{
   int l = 6;
   int ne = 1;
   int sum = 0;
   int k = 0;
-  for (int i = 0; i < l; i++){
-      if (a[i] > 60){
-	  a[i] = 0;
-	}else{
-	  for (int j = 0; j < l; j++){
-	      if (a[j] == 0){
-		  a[j] = a[i];
-		  a[i] = 0;
-		  break;
-		        }
-	        }
-	    }
-    }for (int i = 0; i < l; i++){
-      if (a[0] == 45){
+for (int i = 0; i < l; i++){
+      if (a[i] == 45){
 	  ne = -1;
 	}if (a[i] > 48 && a[i] < 60){
 	  sum = k * 10 + a[i] - 48;
