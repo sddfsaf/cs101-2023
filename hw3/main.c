08a1db8666319621a53a826acf53443dbc818462
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2023-04-05 11:23:35 +0800

    Update main.c

--- hw3/main.c
+++ hw3/main.c
@@ -19,14 +19,14 @@ int main()
     printf("買幾張彩票(1-5):");
     scanf("%d",&l);
     int cout[n];
-    char read[1000];
+    char read[10];
     FILE* fp;
-    char filename[1000];
+    char filename[10000];
     int i =00001;
     time_t curtime;
     FILE* lfp;
     int b =0;
-    lotto_record_t p;
+    lotto_record_t p[200];
     
         if ( l ==0){
         lfp = fopen("record.bin", "r");
@@ -39,16 +39,17 @@ int main()
     scanf("%d%d%d",&a,&b,&c);
     for (d =0;d <5; d++){
         for ( e =0; e<7; e++){
-            if (a ||b ||c ==p.lotto_set[d][e]){
-                while( fread( read, sizeof(read), 1, lfp)){
-                    printf("彩券 No.%d",p.receipt_id);
-                    printf("售出時間：%s",p.receipt_time);
+            if (a ||b ||c ==p->lotto_set[d][e]){
+                while( fread( &p, sizeof(read), 1, lfp)){
+                    printf("彩券 No.%d\n",p->receipt_id);
+                    printf("售出時間：%s\n",p->receipt_time);
                     printf("彩票號碼");
                     for (d =0;d <5; d++){
                         for ( e =0; e<7; e++){
-                          printf("%d",p.lotto_set[d][e]);  
+                          printf("%d ",p->lotto_set[d][e]);  
                         };
                     };
+                    printf("\n");
                 };
             };
         };
@@ -68,13 +69,11 @@ int main()
     srand((unsigned) time(NULL));
     fprintf(fp, "%s", ctime(&curtime));
     fprintf(fp, "No.%d\n", i);
-    lotto_record_t p;
-        p.receipt_id = i;
-        p.receipt_price = l*100;
-        strcpy( p.receipt_time, ctime(&curtime));
-        fprintf(lfp,"%d",p.receipt_id );
-        fprintf(lfp,"%d",p.receipt_price );
-        fprintf(lfp,"%s",p.receipt_time );
+    
+        p->receipt_id = i;
+        p->receipt_price = l*100;
+        strcpy( p->receipt_time, ctime(&curtime));
+        
     for (int b =1; b <=l; b++){
         for ( int k =0;k <n; k++){
             if ( k ==n - 1){
@@ -98,21 +97,24 @@ int main()
         for(int a =0; a <n; a++){
             fprintf(fp, "%d ", cout[a]);
             
-            p.lotto_set[b][a] = cout[a];
-            fwrite(p.lotto_set,sizeof(p.lotto_set), 1, lfp);
+            p->lotto_set[b-1][a] = cout[a];
+            
         }
         fprintf(fp, "\n");
+        
     };
+    for ( int g =0; g<100; g++){
+        fwrite(p+g,sizeof(lotto_record_t), 1, lfp);
+    }
     
     
     
-    fclose (lfp);
-    fclose (fp);
         }else{
             printf("error");
             }
     }
-    
+    fclose (lfp);
+    fclose (fp);
     
     
    
