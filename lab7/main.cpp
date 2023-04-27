655b85dc722f6030da7480a11f478089d957c262
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2023-04-27 14:43:34 +0800

    Update main.cpp

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -10,11 +10,13 @@ class Car{
     string m_model;
     int m_year;
     int m_MaxSeating;
-    Car(string x, string y, int z, int s){
+    string m_DriveMode;
+    Car(string x, string y, int z, int s, string d){
         m_brand = x;
         m_model = y;
         m_year = z;
         m_MaxSeating = s;
+        m_DriveMode = d;
     }
     int get_m_MaxSeating(){
         return m_MaxSeating;
@@ -22,47 +24,26 @@ class Car{
 };
 
 class BMW_Car : public Car{
-    private:
-    string m_DriveMode;
     
     public:
-    BMW_Car(string y, int z, int s) : Car("BMW", y, z, s) {
+    BMW_Car(string y, int z, int s) : Car("BMW", y, z, s, "Rear-wheel") {
         cout << "Constructing BMW_Car\n";
-        m_DriveMode = "Rear-wheel";
-    }
-    
-    string get_DriveMode(){
-        return m_DriveMode;
     }
 };
 
 class AUDI_Car : public Car{
-    private:
-    string m_DriveMode;
     
     public:
-    AUDI_Car(string y, int z, int s) : Car("ADUI", y, z, s) {
+    AUDI_Car(string y, int z, int s) : Car("ADUI", y, z, s, "Front-wheel") {
         cout << "Constructing ADUI_Car\n";
-        m_DriveMode = "Front-wheel";
-    }
-    
-    string get_DriveMode(){
-        return m_DriveMode;
     }
 };
 
 class BENZ_Car : public Car{
-    private:
-    string m_DriveMode;
     
     public:
-    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s) {
+    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s, "Front-wheel") {
         cout << "Constructing BENZ_Car\n";
-        m_DriveMode = "Front-wheel";
-    }
-    
-    string get_DriveMode(){
-        return m_DriveMode;
     }
 };
 
@@ -70,13 +51,13 @@ int main()
 {
     BMW_Car car_1("X5", 2023, 6);
     cout << car_1.m_brand;
-    cout <<" :Drive Mode = " << car_1.get_DriveMode() <<endl;
+    cout <<" :Drive Mode = " << car_1.m_DriveMode <<endl;
     AUDI_Car car_2("X5", 2023, 7);
     cout << car_2.m_brand; 
-    cout <<" :Drive Mode = " << car_2.get_DriveMode() <<endl;
+    cout <<" :Drive Mode = " << car_2.m_DriveMode <<endl;
     BENZ_Car car_3("X5", 2022, 10);
     cout << car_3.m_brand;
-    cout <<" :Drive Mode = " << car_3.get_DriveMode() <<endl;
+    cout <<" :Drive Mode = " << car_3.m_DriveMode <<endl;
 
     return 0;
 }
